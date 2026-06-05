d5196e2b519e03f404c96cd187fc4bfbfe577f48
Mausimux <56847436+Mausimux@users.noreply.github.com>
2026-06-06 01:22:24 +0200

    Removed unnecesary for loop

--- hyprlua/bind.lua
+++ hyprlua/bind.lua
@@ -1,8 +1,5 @@
 bind = function(keys, dispatcher, opts)
 	if type(keys) == "table" then
-		for i, v in ipairs(keys) do
-			keys[i] = tostring(v)
-		end
 		keys = table.concat(keys, " + ")
 	end
 	hl.bind(keys, dispatcher, opts)
