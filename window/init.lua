8088e57c9f62bdb7c8559f4a2d0337fbd7c47b9d
Mausimux <56847436+Mausimux@users.noreply.github.com>
2026-06-06 18:45:17 +0200

    window module

---
+++ window/init.lua
@@ -0,0 +1,7 @@
+require('window.move')
+require('window.resize')
+
+bind({main_mod, 'C'}, hl.dsp.window.close())
+
+bind({main_mod, 'F'}, hl.dsp.window.float({ action = "toggle" }))
+
---
+++ window/move.lua
@@ -0,0 +1,13 @@
+local directions = {
+	H = 'left',
+	J = 'down',
+	K = 'up',
+	L = 'right',
+}
+
+for key, direction in pairs(directions) do
+	bind({main_mod, key}, hl.dsp.focus({ direction = direction}))
+	bind({main_mod, 'SHIFT', key}, hl.dsp.window.move({ direction = direction}))
+end
+
+bind({main_mod, left_click_drag}, hl.dsp.window.drag(), { mouse = true })
