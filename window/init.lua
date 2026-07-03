bf1b38ef03d03d9a71019cfade574baeb81b649a
Mausimux <56847436+Mausimux@users.noreply.github.com>
2026-07-04 01:32:03 +0200

    Window float action removed

--- window/init.lua
+++ window/init.lua
@@ -3,5 +3,5 @@ require('window.resize')
 
 bind({main_mod, 'C'}, hl.dsp.window.close())
 
-bind({main_mod, 'F'}, hl.dsp.window.float({ action = 'toggle' }))
+bind({main_mod, 'F'}, hl.dsp.window.float())
 
