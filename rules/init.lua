933dc90c5a26d52da84123b286954dc6500bc9ca
Mausimux <56847436+Mausimux@users.noreply.github.com>
2026-07-13 19:18:48 +0200

    No gaps nor border on single window

--- rules/init.lua
+++ rules/init.lua
@@ -1,1 +1,2 @@
 require('rules.windows')
+require('rules.smart_gaps')
---
+++ rules/smart_gaps.lua
@@ -0,0 +1,2 @@
+hl.workspace_rule({ workspace = 'w[tv1]', gaps_in = 0, gaps_out = 0 })
+hl.window_rule({ match = { float = false, workspace = 'w[tv1]' }, border_size = 0 })
